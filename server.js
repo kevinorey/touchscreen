65ad8d11f2cf7395046f13e62d53cbe6a63cbf75
kevinorey <kevin.orey@gmail.com>
2018-02-22 19:12:21 -0600

    nodejs

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express')
+const express = require('express');
 const app = express()
+const bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index');
 })
 
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
