49ee74f6a3c6723c4d3b116a779aa50bec4492e0
kevinorey <kevin.orey@gmail.com>
2018-02-24 17:33:16 -0600

    node js

--- barmate.js
+++ barmate.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 
-const apiKey = '8dc5e70e3a07611d59bbc7e14ebf370e';
-
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
@@ -13,11 +11,18 @@ app.get('/', function (req, res) {
   res.render('index', {data: null, error: null});
 })
 
+app.get('/recipe/recipe', (req, res) => {
+    //res.render('recipe', {recipe: null, error: null});
+    var id = req.param('id');
+    console.log('recipe id = ', id);
+    res.render('recipe', {drinkId: id});
+});
+
 app.post('/', function (req, res) {
-  //let city = req.body.city;
+  
   let liquorType = req.body.liquorType;
   console.log('liquor type = ', liquorType);
-  //let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+ 
   let url = `http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${liquorType}`
   
   console.log('url', url);
@@ -37,7 +42,6 @@ app.post('/', function (req, res) {
       if(data.drinks == undefined){
         res.render('index', {data: null, error: 'Returned data is invalid'});
       } else {
-        let dataText = `It's ${data.drinks[1].strDrink} with ID ${data.drinks[1].idDrink}!`;
         res.render('index', {data: data.drinks, error: null});
       }
     }
