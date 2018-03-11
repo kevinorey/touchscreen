144730bec337e7a446a04ecba7ccc4028780fd88
kevinorey <kevin.orey@gmail.com>
2018-03-11 09:49:34 -0500

    more node js

--- barmate.js
+++ barmate.js
@@ -77,81 +77,6 @@ app.get('/recipe/recipe', (req, res) => {
    
 });
 
-//app.post('/', function (req, res) {
-  
-  //let liquorType = req.body.liquorType;
-  //console.log('liquor type = ', liquorType);
-  
-  //let drinkName = req.body.drinkName;
-  //console.log('drink name = ', drinkName);
-  
-  //if ( liquorType != undefined)
-  //{
- 
-	//let url = `http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${liquorType}`
-  
-	//console.log('url', url);
-
-	//request(url, function (err, response, body) {
-	 
-	//console.log('body = <', body, '>');
-	//console.log('error: =<', err, '>');
-	 
-    //if(err){
-      //res.render('liquorSearch', {data: null, error: 'Failed to call url'});
-      //console.log('error:', err);
-      //console.log('body:', body);
-    //} else {
-	  //if ( body == null )
-	  //{
-		  //console.log('Null body returned');
-		  //res.render('recipe', {data: null, error: 'Invalid liquor type'});
-		  //console.log('error:', err);
-          //console.log('body:<', body, '>');
-	  //}
-	  //else
-	  //{
-		//console.log('body is not null... checking undefined');
-		  
-		//if ( body == undefined)
-		//{
-			//console.log('undefined returned body check');
-		    //res.render('recipe', {data: null, error: 'Invalid liquor type'});
-		    //console.log('error:', err);
-            //console.log('body:', body);
-		//}
-		
-		//console.log('index of = ', body.indexOf( '' ));
-		
-		//if ( body == '' )
-		//{
-			//console.log('Blank string returned');
-		    //res.render('liquorSearch', {data: null, error: 'Invalid liquor type'});
-		    //console.log('error:', err);
-            //console.log('body:', body);
-		//}
-		//else
-		//{
-			//console.log('valid body going to parse json');
-			//let data = JSON.parse(body)
-			//console.log('body:', data);
-			//if(data.drinks == undefined){
-				//res.render('liquorSearch', {data: null, error: 'Returned data is invalid'});
-			//} else {
-				//res.render('liquorSearch', {data: data.drinks, error: null});
-			//}
-	    //}
-      //}
-    //}
-  //});
-//}
-
-//else if ( drinkName != undefined)
-//{
-	//console.log('Search by drink name specified');
-//}
-//})
-
 app.post('/', function (req, res) {
   
   let liquorType = req.body.liquorType;
@@ -185,11 +110,11 @@ app.post('/', function (req, res) {
   console.log('body = <', body, '>');
   console.log('error: =<', err, '>');
 	 
-if(err){
-  res.render(renderPage, {data: null, error: 'Failed to call url'});
-  console.log('error:', err);
-  console.log('body:', body);
-} else {
+  if(err){
+	  res.render(renderPage, {data: null, error: 'Failed to call url'});
+	  console.log('error:', err);
+	  console.log('body:', body);
+	} else {
   if ( body == null )
   {
 	  console.log('Null body returned');
